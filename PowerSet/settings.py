58f2c376f55dd2dfde2ea4d414410be0b0623825
DCaliari <caliaridaniele@gmail.com>
2020-09-10 08:31:07 +0100

    Insert Fullscreen Mode TODO: hide instructions and bars.

--- PowerSet/settings.py
+++ PowerSet/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	# added by me
+	'Choice.custom_moduli.MyMiddleware.MyMiddleware'
 ]
 
 ROOT_URLCONF = 'PowerSet.urls'
